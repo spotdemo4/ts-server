53cece5b1d0ea23023afba45555a7927865537af
trev <me@trev.xyz>
2025-07-23 06:54:32 -0400

    style: flake-utils

--- flake.nix
+++ flake.nix
@@ -11,7 +11,12 @@
   };
 
   inputs = {
+    systems.url = "systems";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
     nur = {
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -28,34 +33,19 @@
 
   outputs = {
     nixpkgs,
+    utils,
     nur,
     semgrep-rules,
     ts-web,
     ...
-  }: let
-    build-systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "aarch64-darwin"
-    ];
-    forSystem = f:
-      nixpkgs.lib.genAttrs build-systems (
-        system:
-          f {
-            inherit system;
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [nur.overlays.default];
-            };
-          }
-      );
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [nur.overlays.default];
+      };
 
-    ts-server = forSystem ({
-      pkgs,
-      system,
-      ...
-    }:
-      pkgs.buildGoModule (finalAttrs: {
+      ts-server = pkgs.buildGoModule (finalAttrs: {
         pname = "ts-server";
         version = "0.0.20";
         src = ./.;
@@ -75,10 +65,9 @@
           license = pkgs.lib.licenses.mit;
           platforms = pkgs.lib.platforms.all;
         };
-      }));
-  in rec {
-    devShells = forSystem ({pkgs, ...}: {
-      default = pkgs.mkShell {
+      });
+    in rec {
+      devShells.default = pkgs.mkShell {
         packages = with pkgs; [
           git
           pkgs.nur.repos.trev.bumper
@@ -110,97 +99,72 @@
           prettier
           docker-client
         ];
-        shellHook = ''
-          echo "nix flake check --accept-flake-config" > .git/hooks/pre-push
-          chmod +x .git/hooks/pre-push
-        '';
+        shellHook = pkgs.nur.repos.trev.shellhook.ref;
       };
-    });
 
-    checks = forSystem ({
-      pkgs,
-      system,
-      ...
-    }:
-      pkgs.nur.repos.trev.lib.mkChecks {
-        lint = {
-          src = ./.;
-          nativeBuildInputs = with pkgs; [
-            revive
-            sqlfluff
-            alejandra
-            renovate
-            action-validator
-            prettier
-          ];
-          checkPhase = ''
-            revive -config revive.toml -set_exit_status ./...
-            sqlfluff lint
-            alejandra -c .
-            renovate-config-validator
-            action-validator .github/workflows/*
-            action-validator .gitea/workflows/*
-            action-validator .forgejo/workflows/*
-            prettier --check .
-          '';
-        };
-
-        scan = {
-          src = ./.;
-          nativeBuildInputs = [
-            pkgs.nur.repos.trev.opengrep
-          ];
-          checkPhase = ''
-            mkdir -p "$TMP/scan"
-            HOME="$TMP/scan"
-            opengrep scan --quiet --error --config="${semgrep-rules}/go"
-          '';
-        };
-
-        db = {
-          src = ./.;
-          nativeBuildInputs = with pkgs; [
-            sqlite
-            dbmate
-          ];
-          checkPhase = ''
-            export DATABASE_URL=sqlite:$TMP/check.db
-            dbmate up
-          '';
-        };
-      }
-      // {
-        build = ts-server."${system}".overrideAttrs {
-          doCheck = true;
-          installPhase = ''
-            touch $out
-          '';
-        };
-
-        shell = devShells."${system}".default;
-      });
-
-    formatter = forSystem ({pkgs, ...}: pkgs.alejandra);
-
-    packages = forSystem (
-      {
-        pkgs,
-        system,
-        ...
-      }:
-        with pkgs.nur.repos.trev.lib; rec {
-          default = ts-server."${system}";
-
-          linux-amd64 = goModuleToPlatform default "linux" "amd64";
-          linux-arm64 = goModuleToPlatform default "linux" "arm64";
-          linux-arm = goModuleToPlatform default "linux" "arm";
-          darwin-arm64 = goModuleToPlatform default "darwin" "arm64";
-          windows-amd64 = goModuleToPlatform default "windows" "amd64";
-
-          linux-amd64-image = goModuleToImage linux-amd64;
-          linux-arm64-image = goModuleToImage linux-arm64;
-          linux-arm-image = goModuleToImage linux-arm;
+      checks =
+        pkgs.nur.repos.trev.lib.mkChecks {
+          lint = {
+            src = ./.;
+            deps = with pkgs; [
+              revive
+              sqlfluff
+              alejandra
+              renovate
+              action-validator
+              prettier
+            ];
+            script = ''
+              revive -config revive.toml -set_exit_status ./...
+              sqlfluff lint
+              alejandra -c .
+              renovate-config-validator
+              action-validator .github/workflows/*
+              action-validator .gitea/workflows/*
+              action-validator .forgejo/workflows/*
+              prettier --check .
+            '';
+          };
+          scan = {
+            src = ./.;
+            deps = [
+              pkgs.nur.repos.trev.opengrep
+            ];
+            script = ''
+              opengrep scan --quiet --error --config="${semgrep-rules}/go"
+            '';
+          };
+          db = {
+            src = ./.;
+            deps = with pkgs; [
+              sqlite
+              dbmate
+            ];
+            script = ''
+              export DATABASE_URL=sqlite:$TMP/check.db
+              dbmate up
+            '';
+          };
         }
-    );
-  };
+        // {
+          build = ts-server.overrideAttrs {
+            doCheck = true;
+          };
+          shell = devShells.default;
+        };
+
+      packages = with pkgs.nur.repos.trev.lib; rec {
+        default = ts-server;
+        linux-amd64 = go.moduleToPlatform default "linux" "amd64";
+        linux-arm64 = go.moduleToPlatform default "linux" "arm64";
+        linux-arm = go.moduleToPlatform default "linux" "arm";
+        darwin-arm64 = go.moduleToPlatform default "darwin" "arm64";
+        windows-amd64 = go.moduleToPlatform default "windows" "amd64";
+        linux-amd64-image = go.moduleToImage linux-amd64;
+        linux-arm64-image = go.moduleToImage linux-arm64;
+        linux-arm-image = go.moduleToImage linux-arm;
+      };
+
+      formatter = pkgs.alejandra;
+    });
 }
